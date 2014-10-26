4432e5546b943c84f27d5bc1a99971a7388e11bd
reyueei <romarie321@gmail.com>
2014-10-25 21:36:25 -0700

    flc script

--- absMax.py
+++ absMax.py
@@ -3,67 +3,69 @@ import sys
 import math
 from operator import itemgetter
 from functools import partial
+x = 5.12
+f_values = []
+def membership(value,center):
+    #square root of square of value
+    output = value*center
+    g_values.append(output)
+    result = sum([g_values[n] for n in range(0,len(g_values))])
+    print(result)
+    outputTap(result)
+
+def outputTap(result):
+    if result >= 60:
+        print("tap 4")
+    elif result > 20 and result < 60:
+        print("tap 3")
+    elif result >= -20 and result <=20:
+        print("tap 2")
+    elif result > -60 and result < -20:
+        print("tap 1")
+    else:
+        print("tap 0")
 
 def trap(a, b, c, d, x):
     first = (x - a) / (b - a)
     second = (d - x) / (d - c)
     return( max(min(first, 1., second), 0.))
-
+        
 def tri(a, b, x):
     a = float(a)
     b = float(b)
     m = (a + b) / 2.
     first = (x - a) / (m - a)
     second = (b - x) / (b - m)
-    return(max(min(first, second), 0.))
+    print(max(min(first, second), 0.))
 
-#Input : Voltage Error
-x = 0.12
+#input definition
+
 largeNeg = trap(-51,-50,-6,-5,x)
+if largeNeg > 0:
+    centerVal = -100
+    membership(largeNeg,centerVal)
+    
+#def neg(x):
 neg = trap(-6,-5,-3,-2,x)
+if neg > 0:
+    centerVal = -50
+    membership(neg,centerVal)
+
+#def zero(x):
 zero = trap(-3,-2,2,3,x)
+if zero > 0:
+    centerVal = 0
+    membership(zero,centerVal)
+
+#def positive(x):
 positive = trap(2,3,5,6,x)
+if positive > 0:
+    centerVal = 50
+    membership(positive,centerVal)
+
+#def largePos(x):
 largePos = trap(5,6,50,51,x)
+if largePos > 0:
+    centerVal = 100
+    membership(largePos,centerVal)
 
-result = max(largeNeg,neg,zero,positive,largePos)
-print(result)
-
-#rules
-if result == largeNeg:
-    largeDown(result)
-if result == neg:
-    print("N")
-if result == zero:
-    print("Z")
-if result == positive:
-    print("P")
-if result == largePos:
-    print("LP")
-
-#define Outputs
-    
-"""    
-def largeDown():
-    return tri(-100, -100, -80)
-def down():
-    return tri(-80,-50,-20)
-def noChage():
-    return tri(-20, 0, 20)
-def up():
-    return tri(20, 50, 80)
-def largeUp():
-    return tri(80,100,100)
-    
-
- return tri(5., 9., size)
-
-
-rules = [
-    (largeNeg, largeDown),
-    (neg, down),
-    (zero, noChange),
-    (positive, up),
-    (largePos, largeUp)
-]
-
-"""
